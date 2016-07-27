2394cdd4ac222ad10b18880079377879fcb3a6f9
Jason Liu <blujay000@gmail.com>
2016-07-27 03:20:47 +0000

    models

--- models/address_book.rb
+++ models/address_book.rb
@@ -1,18 +1,17 @@
-  require_relative 'entry'
-  
+ require_relative 'entry'
+ 
  class AddressBook
-  attr_reader :entries
-  
-  
- def initialize
+    attr_reader :entries
+ 
+     def initialize
      @entries = []
    end
-    
- def add_entry(name, phone_number, email)
      
+     def add_entry(name, phone_number, email)
+     # #9
      index = 0
      entries.each do |entry|
-     
+     # #10
        if name < entry.name
          break
        end
