72cbfdf07fb0d52feefbd7bc1f9cb6b1da00c7e3
Jason Liu <blujay000@gmail.com>
2016-07-27 17:54:35 +0000

    start checkpoint 19

--- models/address_book.rb
+++ models/address_book.rb
@@ -6,18 +6,24 @@
      def initialize
      @entries = []
    end
+   
+     def remove_entry(name, phone_number, email)
+      delete_entry = nil
+      if name == entry.name && phone_number == entry.phone_number && email == entry.email
+     end
+    end 
+       @entry.delete(delete_entry)
+     end 
      
      def add_entry(name, phone_number, email)
-     # #9
      index = 0
      entries.each do |entry|
-     # #10
        if name < entry.name
          break
        end
        index += 1
      end
-     # #11
+     
      entries.insert(index, Entry.new(name, phone_number, email))
    end
  end
